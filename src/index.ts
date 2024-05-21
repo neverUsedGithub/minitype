67c3db7ebf764c34e9a4241a3bfe461d2d477f4e
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2024-05-21 18:37:23 +0200

    export stringifyError

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { minLength } from "./validators/minLength";
 export { maxLength } from "./validators/maxLength";
 export { optional } from "./validators/optional";
 export { instance } from "./validators/instance";
+export { BaseValidator, stringifyError } from "./validators/base";
 export { boolean } from "./validators/boolean";
 export { literal } from "./validators/literal";
 export { partial } from "./validators/partial";
@@ -15,4 +16,3 @@ export { regex } from "./validators/regex";
 export { tuple } from "./validators/tuple";
 
 export type { ValidationError, Result, Infer as infer } from "./types";
-export type { BaseValidator } from "./validators/base";
